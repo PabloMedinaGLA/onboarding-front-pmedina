886d50c3b725a8b3c16a2186acdf37e8a736223b
PabloMedinaGLA <pmedina@andreani.com>
2023-05-03 10:40:52 -0300

    incorporacion de yup a validaciones de formulario

--- src/pages/Pedido/Crear/components/index.tsx
+++ src/pages/Pedido/Crear/components/index.tsx
@@ -2,57 +2,51 @@ import { IPedidoPost } from "domain/Pedido";
 
 import { Alert, Button, Input } from "@architecture-it/stylesystem";
 import { Box, Typography } from "@mui/material";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { faCheckCircle } from "@fortawesome/pro-solid-svg-icons";
 import { useState } from "react";
 import usePedido from "app/pedido/use-pedido";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 import styles from "../components/index.module.scss";
 
 export default function Form() {
-  const [boolSubmit, setBoolSubmit] = useState("default");
-  const [cerrar, setCerrar] = useState(true);
+  const [cerrar, setCerrar] = useState(false);
   const { uploadPedido } = usePedido();
+
+  const validationSchema = yup.object({
+    cuentaCorriente: yup
+      .number()
+      .required("Campo requerido")
+      .typeError("Ingrese caracteres Numericos"),
+    codigoDeContratoInterno: yup
+      .number()
+      .required("Campo requerido")
+      .typeError("Ingrese caracteres Numericos"),
+  });
 
   const {
     register,
     handleSubmit,
-    getValues,
     formState: { errors },
-  } = useForm<IPedidoPost>();
-
-  const expresion = /^[0-9]*$/;
-
-  const onSubmit = handleSubmit((values: IPedidoPost) => {
-    if (expresion.test(values.cuentaCorriente) && expresion.test(values.codigoDeContratoInterno)) {
-      uploadPedido(values);
-      setBoolSubmit("enviado");
-    } else {
-      setBoolSubmit("rechazado");
-      setCerrar(true);
-    }
+  } = useForm({
+    resolver: yupResolver(validationSchema),
   });
+
+  const onSubmit = (values: FieldValues) => {
+    const pedido: IPedidoPost | any = {
+      cuentaCorriente: values.cuentaCorriente.toString(),
+      codigoDeContratoInterno: values.codigoDeContratoInterno,
+    };
+
+    setCerrar(true);
+    uploadPedido(pedido);
+  };
 
   return (
     <section className={styles.root}>
-      {boolSubmit == "rechazado" && (
-        <Alert
-          color="error"
-          iconProps={{
-            icon: faCheckCircle,
-          }}
-          open={cerrar}
-          variant="outlined"
-          onCloseProp={() => {
-            setCerrar(false), setBoolSubmit("default");
-          }}
-        >
-          <Typography variant="body2">
-            No puedo ser enviado su <strong>Pedido </strong>
-          </Typography>
-        </Alert>
-      )}
-      {boolSubmit == "enviado" && (
+      {cerrar && (
         <Alert
           color="success"
           iconProps={{
@@ -61,35 +55,47 @@ export default function Form() {
           open={cerrar}
           variant="outlined"
           onCloseProp={() => {
-            setCerrar(false), setBoolSubmit("default");
+            setCerrar(false);
           }}
         >
           <Typography variant="subtitle2">
-            Nuevo Pedido <strong>fue creado con éxito, </strong>
+            Nuevo Pedido <strong>fue creado con éxito </strong>
           </Typography>
         </Alert>
       )}
 
-      <form action="" className={styles.container} onSubmit={onSubmit}>
+      <form
+        action=""
+        className={styles.container}
+        onSubmit={handleSubmit((data) => {
+          onSubmit(data);
+        })}
+      >
         <h1>Registro Pedido</h1>
-        <Box marginTop="var(--spacing-4)" sx={{ width: "40%" }}>
+        <Box marginTop="var(--spacing-4)" sx={{ width: "1", height: "50px" }} textAlign="center">
           <Input
             {...register("cuentaCorriente", {
               required: true,
-              validate: (value) => value === getValues("cuentaCorriente"),
             })}
+            error={Boolean(errors.cuentaCorriente)}
+            helperText={errors.cuentaCorriente ? (errors.cuentaCorriente.message as string) : ""}
             label=""
             placeholder="Cuenta Corriente"
             sx={{ width: 1 }}
             onChange={() => {}}
           />
         </Box>
-        <Box marginTop="var(--spacing-6)" sx={{ width: "40%" }}>
+        <Box marginTop="var(--spacing-6)" sx={{ width: "1", height: "50px" }}>
           <Input
             {...register("codigoDeContratoInterno", {
               required: true,
-              validate: (value) => value === getValues("codigoDeContratoInterno"),
             })}
+            error={Boolean(errors.codigoDeContratoInterno)}
+            helperText={
+              errors.codigoDeContratoInterno
+                ? (errors.codigoDeContratoInterno.message as string)
+                : ""
+            }
             label=""
             placeholder={"Codigo Contrato Interno"}
             sx={{ width: "100%" }}
